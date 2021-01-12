a27fb831c54752a66e682820bbf45ba5d5c9ffaa
Feras <fhathaf@gmail.com>
2021-01-12 14:59:18 +0000

    Setup LFS, updated git ignore and made the project
    Also created a simple environment in the scene and made prefabs from this

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+            body.AddRelativeForce(Vector3.up);
+        }        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("A pressed");
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("D pressed");
+        }
+    }
+}
